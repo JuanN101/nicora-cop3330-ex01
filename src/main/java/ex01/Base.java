e9ab55dbd39a0f2877908565ad5d38a37ce36583
Juan <jfnicora@hotmail.com>
2021-06-06 15:55:58 -0400

    Commit

--- src/main/java/ex01/Base.java
+++ src/main/java/ex01/Base.java
@@ -1,6 +1,11 @@
 package ex01;
 
 /*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Juan Nicora
+ */
+
+/*
 Exercise 1 - Saying Hello
 Create a program that prompts for your name and prints a greeting using your name.
 
@@ -12,8 +17,18 @@ Constraint
 Keep the input, string concatenation, and output separate.
  */
 
+import java.util.Scanner;
+
 public class Base {
     public static void main(String[] args) {
-        System.out.print("What is your name?");
+        System.out.print("What is your name? ");
+
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+
+        String x = "Hello, "+ name +", nice to meet you!";
+
+        System.out.println(x);
+
     }
 }
